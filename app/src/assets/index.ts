ca2692c678593ea128c4bd33b5cfdaae6e0828fc
Andrew Huang <andrewhuang427@gmail.com>
2025-04-19 14:30:17 -0700

    add link to github

--- app/src/assets/index.ts
+++ app/src/assets/index.ts
@@ -1,3 +1,3 @@
-export { default as squareLogo } from "./square-logo.png";
 export { default as googleLogo } from "./google.png";
 export { default as openaiLogo } from "./openai.png";
+export { default as squareLogo } from "./square-logo.png";
