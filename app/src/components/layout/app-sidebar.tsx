49dd7908c15f61fe76fb9c099f08f5af2bd778ea
Andrew Huang <andrewhuang427@gmail.com>
2025-04-27 00:51:34 -0700

    update condition for rendering loading sidebar group

--- app/src/components/layout/app-sidebar.tsx
+++ app/src/components/layout/app-sidebar.tsx
@@ -39,7 +39,7 @@ export default function AppSidebar() {
         <div className="grow" />
       </SidebarHeader>
       <SidebarContent className="overflow-hidden">
-        {isLoading ? (
+        {isLoading && user == null ? (
           <LoadingSidebarGroup />
         ) : user == null ? (
           <SignInSidebarGroup />
